4af59bef3ce56aab42e4a81d6bab75e49adf008d
Elitesun <elitesun.dev@gmail.com>
2024-11-11 22:10:18 +0000

    Fix build script to navigate to the correct directory

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Navigate to the directory containing manage.py
+cd weather
+
 python manage.py collectstatic --no-input
 python manage.py migrate 
