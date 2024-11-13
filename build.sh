3416824c54a6f631cf813dedba230ddadc11c880
Elitesun <elitesun.dev@gmail.com>
2024-11-13 22:34:04 +0000

    build

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Navigate to the directory containing manage.py
-cd weather
 
 python manage.py collectstatic --no-input
 python manage.py migrate 
 
-cd weather
