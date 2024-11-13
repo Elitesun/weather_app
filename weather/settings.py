801cb10aa21bc1d702808471a02ccadd737cf8eb
Elitesun <elitesun.dev@gmail.com>
2024-11-13 22:54:41 +0000

    nada

--- weather/settings.py
+++ weather/settings.py
@@ -30,8 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS =os.getenv('ALLOWED_HOSTS', '').split(',')# Not recommended for production, but okay for this demo
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
